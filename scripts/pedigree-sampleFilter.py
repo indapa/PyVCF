df2235fd7d669a5fb6f5516b88036ebbfcc7c313
indapa <indapa@gmail.com>
2014-01-23 17:11:47 -0500

    added --invert and --no-invert options and generate output vcf filename based on basename of *.ped and *.vcf.gz basenames

--- scripts/pedigree-sampleFilter.py
+++ scripts/pedigree-sampleFilter.py
@@ -1,12 +1,41 @@
+
 from vcf import SampleFilter
 from PedPy import Ped, Pedfile
-pedfile='/Users/indapa/Research/SequencingData/Pedfiles/Families/003.ped'
-vcfile='/Users/indapa/Research/SequencingData/VCFs/C868.vcf.gz'
-outfile='filter.vcf'
-pedobj=Pedfile(pedfile)
-pedobj.parsePedfile()
-keepList=pedobj.returnIndivids()
-keepstring=",".join(keepList)
+from common import return_file_basename
+import sys
+import os
+import argparse
 
 
-sf = SampleFilter(infile=vcfile, outfile=outfile, filters=keepstring, invert=True)
+
+
+def main():
+    """ Perform sample filtering based based on samples read from a *.ped file and *.vcf.gz file """
+    usage = "usage: %prog [options]"
+    parser = argparse.ArgumentParser(description='Given a gzipped vcf file and pedigree file, generate a new vcf with only those samples present in the pedigree (ped file) ')
+    
+    parser = argparse.ArgumentParser(description='Program description')
+    parser.add_argument('-ped', dest='pedfile', type=str, help="*.ped file")
+    parser.add_argument('vcfile',  type=str,help='*.vcf.gz file')
+    parser.add_argument('--invert',dest='invert',action='store_true', help="remove samples not in the pedigree from the vcf")
+    parser.add_argument('--no-invert',dest='invert',action='store_false', help="remove samples in the pedigree from the vcf")
+    args=parser.parse_args()
+    
+    pedigree_basename=return_file_basename(args.pedfile)
+    vcf_basename = return_file_basename(return_file_basename(args.vcfile))
+    
+    
+    outfile=".".join([vcf_basename, pedigree_basename,'vcf'])
+    
+    pedobj=Pedfile(args.pedfile)
+    pedobj.parsePedfile()
+    keepList=pedobj.returnIndivids()
+    keepstring=",".join(keepList)
+
+
+    sf = SampleFilter(infile=args.vcfile, outfile=outfile, filters=keepstring, invert=True)
+    
+
+if __name__ == "__main__":
+    main()
+
